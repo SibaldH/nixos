7b56bd8be13a1ce10064969214d7f179f401c8dc
SibaldH <sibald.hulselmans@protonmail.com>
2025-09-13 16:22:36 +0200

    change package to use flake inputs

--- nixos/modules/wm.nix
+++ nixos/modules/wm.nix
@@ -1,7 +1,10 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, inputs, ... }: {
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
+    withUWSM = true;
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
 
   environment.systemPackages = with pkgs; [
