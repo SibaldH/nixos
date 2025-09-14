fa55c6c468050c4099100010e1763e626955c66f
SibaldH <sibald.hulselmans@protonmail.com>
2025-09-14 20:25:53 +0200

    add xkeuboardconfig

--- nixos/modules/keyboard.nix
+++ nixos/modules/keyboard.nix
@@ -1,15 +1,19 @@
 { pkgs, ...}: {
+  environment.systemPackages = with pkgs; [
+    xorg.xkeyboardconfig
+  ];
+
   console = {
     font = pkgs.nerd-fonts.jetbrains-mono;
     keyMap = "be-latin1";
     useXkbConfig = false; # use xkb.options in tty.
   };
 
-  services.xserver = {
-    enable = true;
-    xkb = {
-      layout = "be";
-      variant = "latin1";
-    };
-  };
+  # services.xserver = {
+  #   enable = true;
+  #   xkb = {
+  #     layout = "be";
+  #     variant = "latin1";
+  #   };
+  # };
 }
