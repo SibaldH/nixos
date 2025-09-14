db38fd7592c28c93006beb795e7ad9c210493be6
SibaldH <sibald.hulselmans@protonmail.com>
2025-09-14 15:59:45 +0200

    add monitor config

---
+++ home-manager/modules/hyprland/monitor.nix
@@ -0,0 +1,5 @@
+{
+  imports = [
+    ./monitors/home-flipped.nix
+  ];
+}
---
+++ home-manager/modules/hyprland/monitors/home-flipped.nix
@@ -0,0 +1,7 @@
+{ ... }: {
+  wayland.windowManager.hyprland.settings = {
+    monitor = [
+      "eDP-1,2880x1920@120,0x0,1"
+    ];
+  };
+}
