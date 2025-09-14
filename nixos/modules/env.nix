fc0297c051533b3549eda0262bdfa4b89a07b2ca
SibaldH <sibald.hulselmans@protonmail.com>
2025-09-14 20:12:30 +0200

    set XDG_CONFIG_ROOT env var

--- nixos/modules/env.nix
+++ nixos/modules/env.nix
@@ -1,13 +1,14 @@
-{user, ...}: {
+{user, pkgs, ...}: {
 	environment.sessionVariables = rec {
 		TERMINAL = "alacritty";
 		EDITOR = "nvim";
-		XDG_BIN_HOME = "$HOME/.local/bin";
 		PATH = [
 			"${XDG_BIN_HOME}"
 		];
 		NH_FLAKE = "/home/${user}/nixos";
     NIXOS_OZONE_WL = "1";
+		XDG_BIN_HOME = "$HOME/.local/bin";
+    XDG_CONFIG_ROOT = "${pkgs.xorg.xkeyboardconfig}/share/X11/xkb";
     XDG_SESSION_TYPE = "wayland";
     XDG_CURRENT_DESKTOP = "Hyprland";
     XDG_SESSION_DESKTOP = "Hyprland";
