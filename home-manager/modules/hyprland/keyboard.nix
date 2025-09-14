a40469465777bc30cf4e4cc324fd54cb3719154d
SibaldH <sibald.hulselmans@protonmail.com>
2025-09-14 21:12:11 +0200

    fix error

--- home-manager/modules/hyprland/keyboard.nix
+++ home-manager/modules/hyprland/keyboard.nix
@@ -1,22 +1,17 @@
-{
+{ config, pkgs, ... }:{
   wayland.windowManager.hyprland.settings = {
-    settings = {
-      input = {
-        kb_layout = "be";
-        kb_variant = "nodeadkeys";
-        kb_options = "ctrl:nocaps,compose:rctrl";
-        numlock_by_dault = true;
-        mouse_refoces = false;
+    input = {
+      kb_layout = "be";
+      kb_variant = "nodeadkeys";
+      kb_options = "ctrl:nocaps,compose:rctrl";
+      numlock_by_default = true;
+      mouse_refocus = false;
 
-        follow_mouse = 1;
-        touchpad = {
-          natural_scroll = "yes";
-          middle_button_emulator = true;
-          clickfinger_behavior = false;
-          scroll_factor = "1.0";
-        };
-        sensitivity = 0;
+      follow_mouse = 1;
+      touchpad = {
+        natural_scroll = true; 
       };
+      sensitivity = 0;
     };
   };
 }
