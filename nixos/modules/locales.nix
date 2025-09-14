94c8367f5b7d05ba4c2e7e2cd10dd83b90162191
SibaldH <sibald.hulselmans@protonmail.com>
2025-09-14 20:11:59 +0200

    setup en_BE.UTF-8 locales

--- nixos/modules/locales.nix
+++ nixos/modules/locales.nix
@@ -1,3 +1,24 @@
-{
-  i18n.defaultLocale = "en_BE.UTF-8";
+{ pkgs, ... }: {
+  environment.systemPackages = with pkgs; [ glibcLocales ];
+
+  i18n = {
+    extraLocales = "all";
+    defaultLocale = "en_BE.UTF-8";
+    extraLocaleSettings = {
+      LANGUAGE = "en_BE.UTF-8";
+      LANG = "en_BE.UTF-8";
+      LC_TIME = "en_BE.UTF-8";
+      LC_NUMERIC = "en_BE.UTF-8";
+      LC_COLLATE = "en_BE.UTF-8";
+      LC_IDENTIFICATION = "en_BE.UTF-8";
+      LC_MONETARY = "en_BE.UTF-8";
+      LC_PAPER = "en_BE.UTF-8";
+      LC_ADDRESS = "en_BE.UTF-8";
+      LC_TELEPHONE = "en_BE.UTF-8";
+      LC_MEASUREMENT = "en_BE.UTF-8";
+      LC_NAME = "en_BE.UTF-8";
+      LC_CTYPE = "en_BE.UTF-8";
+      LC_MESSAGES = "en_BE.UTF-8";
+    };
+  };
 }
