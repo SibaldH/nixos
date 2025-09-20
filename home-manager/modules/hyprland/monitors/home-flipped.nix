4fa5a5eefd33d2a5bb6e390ab8328442689bdf78
SibaldH <sibald.hulselmans@protonmail.com>
2025-09-20 21:41:37 +0200

    include external monitors

--- home-manager/modules/hyprland/monitors/home-flipped.nix
+++ home-manager/modules/hyprland/monitors/home-flipped.nix
@@ -2,6 +2,8 @@
   wayland.windowManager.hyprland.settings = {
     monitor = [
       "eDP-1,2880x1920@120,0x0,1.5"
+      "desc:Philips Consumer Electronics Company 27M1C5500V AU02337004363,2560x1440@120.00,-2560x0,1"
+      "desc:Philips Consumer Electronics Company 27M1C5500V AU02337004359,2560x1440@120.00,-5120x0,1"
     ];
   };
 }
