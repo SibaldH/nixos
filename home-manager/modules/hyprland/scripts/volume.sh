1d4240e8d8a7386a6edcf60e60dfdd452112ce2a
SibaldH <sibald.hulselmans@protonmail.com>
2025-09-20 22:03:18 +0200

    snap to closes 5%

--- home-manager/modules/hyprland/scripts/volume.sh
+++ home-manager/modules/hyprland/scripts/volume.sh
@@ -5,6 +5,12 @@ get_current_volume() {
     pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}' | sed 's/%//'
 }
 
+# Function to round a number to the nearest multiple of 5
+round_to_nearest_5() {
+    local volume=$1
+    echo $(( (volume + 2) / 5 * 5 ))
+}
+
 # Check command line arguments
 if [[ "$#" != 1 || ! ("$1" == "inc" || "$1" == "dec" || "$1" == "mute" ) ]]; then
     printf "Usage: $0 [inc|dec|mute]\n"
@@ -19,11 +25,19 @@ fi
 
 # Perform volume adjustment
 if [[ "$1" == "inc" ]]; then
-    [ "$(get_current_volume)" -lt 150 ] && pactl set-sink-volume @DEFAULT_SINK@ +5%
-    notify-send -t 1000 "Volume" "Increased to $(get_current_volume)%"
+    current_volume=$(get_current_volume)
+    if [ "$current_volume" -lt 150 ]; then
+        new_volume=$((current_volume + 5))
+        rounded_volume=$(round_to_nearest_5 "$new_volume")
+        [ "$rounded_volume" -le 150 ] && pactl set-sink-volume @DEFAULT_SINK@ "$rounded_volume%"
+        notify-send -t 1000 "Volume" "Increased to $rounded_volume%"
+    fi
 elif [[ "$1" == "dec" ]]; then
-    pactl set-sink-volume @DEFAULT_SINK@ -5%
-    notify-send -t 1000 "Volume" "Decreased to $(get_current_volume)%"
+    current_volume=$(get_current_volume)
+    new_volume=$((current_volume - 5))
+    rounded_volume=$(round_to_nearest_5 "$new_volume")
+    [ "$rounded_volume" -ge 0 ] && pactl set-sink-volume @DEFAULT_SINK@ "$rounded_volume%"
+    notify-send -t 1000 "Volume" "Decreaced to $rounded_volume%"
 elif [[ "$1" == "mute" ]]; then
     pactl set-sink-mute @DEFAULT_SINK@ toggle
     notify-send -t 1000 "Volume" "$(pactl get-sink-mute @DEFAULT_SINK@ | grep -q yes && echo Muted || echo Unmuted)"
