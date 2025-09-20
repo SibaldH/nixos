c62a3c5670ae49b5d92c82387df4da2d4b1a1683
SibaldH <sibald.hulselmans@protonmail.com>
2025-09-20 22:07:06 +0200

    optional substituteInPlace phase
    first check if an app is used, before trying to substitute

--- home-manager/modules/hyprland/scripts.nix
+++ home-manager/modules/hyprland/scripts.nix
@@ -1,30 +1,37 @@
 { config, pkgs, ... }:
 
 let
+  # Custom derivation for bundling multiple scripts
   hyprlandScripts = pkgs.stdenv.mkDerivation {
     name = "hyprland-scripts";
-    src = ./scripts;
+    src = ./scripts;  # Path to ./scripts/ dir with .sh files
 
-    buildInputs = with pkgs; [
-      pulseaudio
-      libnotify
-      bash
-    ];
+    buildInputs = with pkgs; [ pulseaudio libnotify bash ];
 
     installPhase = ''
       mkdir -p $out/bin
       for script in $src/*.sh; do
         name=$(basename "$script" .sh)
         install -m755 "$script" $out/bin/$name
-        substituteInPlace $out/bin/$name \
-          --replace-fail '#!/usr/bin/env bash' '#!${pkgs.bash}/bin/bash' \
-          --replace-fail 'pactl' '${pkgs.pulseaudio}/bin/pactl' \
-          --replace-fail 'notify-send' '${pkgs.libnotify}/bin/notify-send'
-          # Add more substitutions if other scripts need them
+        # Only substitute if pattern exists to avoid errors
+        if grep -q '#!/usr/bin/env bash' $out/bin/$name; then
+          substituteInPlace $out/bin/$name \
+            --replace '#!/usr/bin/env bash' '#!${pkgs.bash}/bin/bash'
+        fi
+        if grep -q 'pactl' $out/bin/$name; then
+          substituteInPlace $out/bin/$name \
+            --replace 'pactl' '${pkgs.pulseaudio}/bin/pactl'
+        fi
+        if grep -q 'notify-send' $out/bin/$name; then
+          substituteInPlace $out/bin/$name \
+            --replace 'notify-send' '${pkgs.libnotify}/bin/notify-send'
+        fi
+        # Add more conditional substitutions for other commands as needed
       done
-    ''; 
+    '';
   };
-in {
+in
+{
   home.packages = with pkgs; [
     hyprlandScripts
   ];
