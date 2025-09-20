b5f733109ee6c044b08078f950144b44dd5ff6be
SibaldH <sibald.hulselmans@protonmail.com>
2025-09-20 23:26:02 +0200

    make scripts.nix more readable and less copy paste

--- home-manager/modules/hyprland/scripts.nix
+++ home-manager/modules/hyprland/scripts.nix
@@ -1,32 +1,41 @@
 { config, pkgs, ... }:
 
 let
+  # Mapping of commands to their Nix store paths
+  substitutions = {
+    "#!/usr/bin/env bash" = "#!${pkgs.bash}/bin/bash";
+    "pactl" = "${pkgs.pulseaudio}/bin/pactl";
+    "notify-send" = "${pkgs.libnotify}/bin/notify-send";
+    "brightnessctl" = "${pkgs.brightnessctl}/bin/brightnessctl";
+    "bc" = "${pkgs.bc}/bin/bc";
+    # Add more mappings as needed, e.g., "grim" = "${pkgs.grim}/bin/grim";
+  };
+
   # Custom derivation for bundling multiple scripts
   hyprlandScripts = pkgs.stdenv.mkDerivation {
     name = "hyprland-scripts";
     src = ./scripts;  # Path to ./scripts/ dir with .sh files
 
-    buildInputs = with pkgs; [ pulseaudio libnotify bash ];
+    buildInputs = with pkgs; [
+      bash
+      pulseaudio    # For volume.sh
+      libnotify     # For brightness.sh
+      brightnessctl # For brightness.sh
+      bc            # For brightness.sh
+    ];
 
     installPhase = ''
       mkdir -p $out/bin
       for script in $src/*.sh; do
         name=$(basename "$script" .sh)
         install -m755 "$script" $out/bin/$name
-        # Only substitute if pattern exists to avoid errors
-        if grep -q '#!/usr/bin/env bash' $out/bin/$name; then
-          substituteInPlace $out/bin/$name \
-            --replace '#!/usr/bin/env bash' '#!${pkgs.bash}/bin/bash'
-        fi
-        if grep -q 'pactl' $out/bin/$name; then
-          substituteInPlace $out/bin/$name \
-            --replace 'pactl' '${pkgs.pulseaudio}/bin/pactl'
-        fi
-        if grep -q 'notify-send' $out/bin/$name; then
-          substituteInPlace $out/bin/$name \
-            --replace 'notify-send' '${pkgs.libnotify}/bin/notify-send'
-        fi
-        # Add more conditional substitutions for other commands as needed
+        # Loop over substitutions map
+        ${builtins.concatStringsSep "\n" (builtins.map (pattern: ''
+          if grep -q ${pkgs.lib.escapeShellArg pattern} $out/bin/$name; then
+            substituteInPlace $out/bin/$name \
+              --replace ${pkgs.lib.escapeShellArg pattern} ${pkgs.lib.escapeShellArg (builtins.getAttr pattern substitutions)}
+          fi
+        '') (builtins.attrNames substitutions))}
       done
     '';
   };
