fcc92a8dbc9b87662b60f767a2216ef58edf2527
SibaldH <sibald.hulselmans@protonmail.com>
2025-10-20 13:14:09 +0200

    feat: Add cachix hyprland caching for shorter build time

--- nixos/modules/wm.nix
+++ nixos/modules/wm.nix
@@ -7,6 +7,11 @@
     portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
 
+  nix.settings = {
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
+
   programs.uwsm.enable = true;
 
   environment.systemPackages = with pkgs; [
